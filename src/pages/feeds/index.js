deebcbf4a3466ab836db8ba8cafa738f18cb8d30
SolovevaJulia <j.soloveva87@yandex.ru>
2022-01-05 12:54:33 +0300

    create roating

---
+++ src/pages/feeds/index.js
@@ -0,0 +1,11 @@
+import SearchPanel from "../../components/pages/feeds/search-panel";
+
+const Feeds = () => {
+    return (
+        <section>
+            <SearchPanel />
+        </section>
+    );
+};
+
+export default Feeds;
