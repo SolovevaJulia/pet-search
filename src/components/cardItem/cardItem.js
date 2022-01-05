c95e6a9747ebe1b689cac3c1a5cb6b4c0cdcf57c
SolovevaJulia <j.soloveva87@yandex.ru>
2022-01-05 19:12:17 +0300

    cards

--- src/components/cardItem/cardItem.js
+++ src/components/cardItem/cardItem.js
@@ -1,10 +1,23 @@
+import PetPhoto from "../cardItem/img/photo_2022-01-05_18-00-35.jpg";
+import PetPhoto2 from "../cardItem/img/pet2.jpg";
+import PetPhoto3 from "../cardItem/img/pet3.jpg";
+import "./cards.css";
+
 const CardItem = () => {
     return (
-        <section>
+        <section className="card-section">
             <div className="card-wrapper">
-                <img className="pet-img" crs="" />
+                <img className="pet-img" src={PetPhoto} />
                 <h2>Пропал кот</h2>
             </div>
+            <div className="card-wrapper">
+                <img className="pet-img" src={PetPhoto2} />
+                <h2>Пропал кот</h2>
+            </div>
+            {/* <div className="card-wrapper">
+                <img className="pet-img" src={PetPhoto3} />
+                <h2>Пропал кот</h2>
+            </div> */}
         </section>
     );
 };
