cbbe8f1d8089757e0b39654167bf8a98c61cbb25
DarkLis <coolbiird@gmail.com>
2022-01-09 17:40:52 +0300

    Первый пуш на GitLab Pages

--- src/pages/dev_ilya/index.js
+++ src/pages/dev_ilya/index.js
@@ -31,8 +31,6 @@ const DevIlya = () => {
         }
     }
 
-    // Отправляем все данные на бэкенд
-
     const setFormsFields = (e) => {
         e.preventDefault();
         const objectForm = {
