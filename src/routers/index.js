51aa4d546b44435233ff0774812d50eb9e5e5163
SolovevaJulia <j.soloveva87@yandex.ru>
2022-01-11 14:57:12 +0300

    fix conflict

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,24 +2,24 @@ import { Routes, Route } from "react-router-dom";
 
 //Pages
 import Feeds from "../pages/feeds/index";
-import PostAds from "../pages/post_ads";
-import Home from "../pages/home";
-
-
 //Тест разработка, удалить по завершению проекта!
 import DevIlya from "../pages/dev_ilya";
 //Тест разработка, удалить по завершению проекта!
-
+import PostAds from "../pages/post_ads";
+import Home from "../pages/home";
+import Auth from "../pages/auth";
+import Reg from "../pages/reg";
 const MainRoutes = () => {
     return (
         <Routes>
-            <Route path="pet-search/" element={<Home />}/>
+            <Route path="pet-search/" element={<Home />}></Route>
             <Route path="pet-search/feeds" element={<Feeds />} />
-            <Route path="pet-search/post_ads" element={<PostAds />} />
-
             {/*    Тест разработка, удалить по завершению проекта!*/}
             <Route path="pet-search/dev_ilya" element={<DevIlya />} />
             {/*    Тест разработка, удалить по завершению проекта!*/}
+            <Route path="pet-search/post_ads" element={<PostAds />} />
+            <Route path="pet-search/auth" element={<Auth />} />
+            <Route path="pet-search/reg" element={<Reg />} />
         </Routes>
     );
 };
