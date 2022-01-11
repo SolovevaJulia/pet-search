ccc1d5bbc576bdb9a90624b2709fb41ecd2f39e7
Илья Лесунов <coolbiird@gmail.com>
2022-01-11 13:42:03 +0300

    Кэширование сайта

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,43 @@
+const CACHE = 'network-or-cache-v1';
+const timeout = 400;
 self.addEventListener('install', (event) => {
     console.log('Установлен', event);
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                './iconLogoPWA',
-                './index.html',
-                './style.css'
+                'iconLogoPWA',
+                '/static/css/main.f7c50ba3.css',
+                '/static/js/main.822851b2.js'
             ]);
         })
     );
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Активирован?', event);
+// при событии fetch, мы и делаем запрос, но используем кэш, только после истечения timeout.
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fromNetwork(event.request, timeout)
+        .catch((err) => {
+            console.log(`Error: ${err.message()}`);
+            return fromCache(event.request);
+        }));
 });
 
-self.addEventListener('fetch', (event) => {
-    console.log('Происходит запрос на сервер...', event);
-});
+// Временно-ограниченный запрос.
+function fromNetwork(request, timeout) {
+    return new Promise((fulfill, reject) => {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then((response) => {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+}
+
+function fromCache(request) {
+// Открываем наше хранилище кэша (CacheStorage API), выполняем поиск запрошенного ресурса.
+// Обратите внимание, что в случае отсутствия соответствия значения Promise выполнится успешно, но со значением `undefined`
+    return caches.open(CACHE).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
+}
