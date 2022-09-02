c9a4e187ea8ff6226d7fa28bd0746d8ca66edc9d
Julia Soloveva <yu.soloveva@myhart.ru>
2022-09-02 09:49:31 +0300

    1

--- src/components/Modal-Card.vue
+++ src/components/Modal-Card.vue
@@ -142,7 +142,7 @@
               <div class="flex justify-center items-center w-full">
                 <label
                   for="dropzone-file"
-                  class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                  class="flex flex-col justify-center items-center w-full h-68 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer hover:bg-gray-100"
                 >
                   <div
                     class="flex flex-col justify-center items-center pt-5 pb-6"
