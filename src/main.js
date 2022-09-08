2b77d85965da6d198dcfbf832564e643e5771524
Julia Soloveva <yu.soloveva@myhart.ru>
2022-09-08 17:00:09 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from "vue";
-import Antd from "ant-design-vue";
+import "flowbite";
+// import Antd from "ant-design-vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import "./index.css";
 
 import store from "./store";
-import "ant-design-vue/dist/antd.css";
-import "flowbite";
-createApp(App).use(store).use(Antd).use(router).mount("#app");
+
+// import "ant-design-vue/dist/antd.css";
+createApp(App)
+  .use(store)
+  // .use(Antd)
+  .use(router)
+  .mount("#app");
